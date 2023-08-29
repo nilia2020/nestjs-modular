49b2a3ca97e3b49b437666b4c1abb92bdf4730c4
jorge niglia <jorge.niglia@gmail.com>
2023-08-29 20:50:06 -0300

    jwt token

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
 import * as bcrypt from 'bcrypt';
-
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/services/users.service';
+import { User } from 'src/users/entities/user.entity';
+import { PayloadToken } from '../models/token.model';
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
@@ -22,5 +27,12 @@ export class AuthService {
         throw new NotFoundException(`User password not found`);
       }
     }
+  }
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user,
+    };
   }
 }
