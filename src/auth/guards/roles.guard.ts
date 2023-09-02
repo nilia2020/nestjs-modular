c6567bc78b41458fc4bad101ed40a0e880b66b5e
jorge niglia <jorge.niglia@gmail.com>
2023-09-01 22:28:37 -0300

    roles

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { PayloadToken } from '../models/token.model';
+import { Role } from '../models/roles.model';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles: Role[] = this.reflector.get<Role[]>(
+      ROLES_KEY,
+      context.getHandler(),
+    );
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as PayloadToken;
+    const isAuth = roles.some((role) => role === user.role);
+    if (!isAuth) {
+      throw new ForbiddenException('forbidden your role is wrong');
+    }
+    return isAuth;
+  }
+}
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -15,7 +15,6 @@ import {
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 // import { Response } from 'express';
-import { AuthGuard } from '@nestjs/passport';
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import {
   CreateProductDto,
@@ -23,12 +22,17 @@ import {
   UpdateProductDto,
 } from '../dtos/products.dto';
 import { ProductsService } from './../services/products.service';
-@UseGuards(AuthGuard('jwt'))
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/models/roles.model';
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
-
+  @Public()
   @Get()
   @ApiOperation({ summary: 'List of products' })
   getProducts(@Query() params: FilterProductsDto) {
@@ -39,7 +43,7 @@ export class ProductsController {
   getProductFilter() {
     return `yo soy un filter`;
   }
-
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', ParseIntPipe) productId: number) {
@@ -48,7 +52,7 @@ export class ProductsController {
     // });
     return this.productsService.findOne(productId);
   }
-
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     // return {
